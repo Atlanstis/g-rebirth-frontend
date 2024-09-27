c3f7ba6c2058001a76a0850a848675f0ccc0105a
Gypsophila <futur_wxc@163.com>
2024-09-27 16:16:15 +0800

    feat: 安装 pinia

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,13 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
+import { setupStore } from './stores';
 
 function setup() {
   const app = createApp(App);
 
   app.use(router);
-
+  setupStore(app);
   app.mount('#app');
 }
 
